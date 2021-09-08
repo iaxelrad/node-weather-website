b5bf0ad6342818e86b44bcff36ba010ce0727b02
Itamar Axelrad <itamara@aman.co.il>
2021-09-08 12:35:42 +0300

    removed unused console.log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,6 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=0cf6bac3021525eefe8ad759e556a1cd&query=${latitude},${longitude}`;
   request({ url, json: true }, (error, response) => {
-    console.log(response);
     const { error: responseError, current, location } = response.body;
     if (error) {
       callback('Unable to connect to weather service!', undefined);
